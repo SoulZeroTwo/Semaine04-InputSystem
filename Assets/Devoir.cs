ce903287c46efb5e7c060fa7b6912b2f91a11468
SoulZeroTwo <contactjonathanevoy@gmail.com>
2024-02-16 12:08:23 -0500

    Devoir ajouter

---
+++ Assets/Devoir.cs
@@ -0,0 +1,6 @@
+    // DEVOIR:
+
+    // Des prefabs de cube sont créés et spawn aléatoirement dans une zone au dessus du vaisseau.
+
+    // Trois cubes s'expulsent du cube original quand il touche à terre
+    // Si ces cubes touchent le cube, le vaisseau disparait
